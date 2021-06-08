4edb12b3b0027e02e30ad46c0d208483e33398ff
João Pedro Alves <jotalmeida007@hotmail.com>
2021-06-07 21:27:32 -0300

    Finish initial db setup

---
+++ lib/exmoveit/profile__data.ex
@@ -0,0 +1,26 @@
+defmodule Exmoveit.Profile_Data do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Exmoveit.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "profile_data" do
+    field :current_experience, :decimal
+    field :current_level, :integer
+    field :tasks_completed, :integer
+
+    belongs_to :users, User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(profile__data, attrs) do
+    profile__data
+    |> cast(attrs, [:current_experience, :current_level, :tasks_completed])
+    |> validate_required([:current_experience, :current_level, :tasks_completed])
+  end
+end
