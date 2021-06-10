96d5174bd57c8b4859afeae326f710f02c25649f
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-10 13:52:38 -0300

    add users controller

---
+++ lib/exmoveit_web/controllers/users_controller.ex
@@ -0,0 +1,24 @@
+defmodule ExmoveitWeb.UsersController do
+  use ExmoveitWeb, :controller
+
+  alias Exmoveit.User
+  alias ExmoveitWeb.FallbackController
+
+  action_fallback FallbackController
+
+  @spec create(Plug.Conn.t(), %{email: String.t()}) ::
+          {:error, %{status: :bad_request, result: Ecto.Changeset.t()}}
+          | {:ok, %User{}}
+  @doc """
+  Inserts an user into the database.
+  """
+  def create(conn, %{"email" => _email} = user) do
+    with {:ok, %User{} = user} <- Exmoveit.create_user(user) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    else
+      error -> error
+    end
+  end
+end
