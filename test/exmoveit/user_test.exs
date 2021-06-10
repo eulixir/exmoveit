89cacc14c8216bc083d3fcbe177e27dcedd4601e
João Pedro Alves <jotalmeida007@hotmail.com>
2021-06-10 19:09:48 -0300

    Add User changeset test

---
+++ test/exmoveit/user_test.exs
@@ -0,0 +1,26 @@
+defmodule Exmoveit.UserTest do
+  use Exmoveit.DataCase
+
+  import Exmoveit.Factory
+
+  alias Ecto.Changeset
+
+  alias Exmoveit.User
+
+  describe "changeset/1" do
+    test "when all params are valid, returns a valid changeset" do
+      params = build(:user_params)
+
+      response = User.changeset(params)
+
+      assert %Changeset{
+               changes: %{
+                 image: "src/banana",
+                 name: "Jp",
+                 email: "jp@banana.com"
+               },
+               valid?: true
+             } = response
+    end
+  end
+end
