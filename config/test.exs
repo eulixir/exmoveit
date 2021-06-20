c7a0ae05a0b8d63080b008c307f9206a427184ba
João Pedro Alves <jotalmeida007@hotmail.com>
2021-06-20 02:20:35 -0300

    Update config

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :exmoveit, Exmoveit.Repo,
-  username: System.get_env("DB_USERNAME"),
-  password: System.get_env("DB_PASSWORD"),
+  username: postgres,
+  password: postgres,
   database: "exmoveit_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
